68eb72dff3c67620366309ce5cbaa91ddcaf431b
binitaepa <binitaepa@gmail.com>
2023-10-20 05:55:39 +0600

    delete added

--- index.js
+++ index.js
@@ -89,6 +89,12 @@ async function run() {
         const result = await cardCollection.insertOne(product);
         res.send(result);
     })
+    app.delete('/cards/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await coffeeCollection.deleteOne(query);
+        res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
